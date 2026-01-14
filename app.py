f71741a113b1d804c43cb826fd425cfb528f654f
Luis Rios Alvarado <luismanuelrios123@gmail.com>
2026-01-14 14:57:11 -0500

    Fixed minor errors

--- app.py
+++ app.py
@@ -3,13 +3,6 @@ from data import products
 
 app = Flask(__name__)
 
-# Mock data
-products = [
-    {"id": 1, "name": "Laptop", "price": 899.99, "category": "electronics"},
-    {"id": 2, "name": "Book", "price": 14.99, "category": "books"},
-    {"id": 3, "name": "Desk", "price": 199.99, "category": "furniture"},
-]
-
 # TODO: Implement homepage route that returns a welcome message
 @app.route("/")
 def home():
@@ -17,7 +10,7 @@ def home():
     "resource_endpoint": "/products"}), 200
 
 # TODO: Implement GET /products route that returns all products or filters by category
-@app.route("/products")
+@app.route("/products", methods=["GET"])
 def get_products():
     category = request.args.get("category")
     if category:
@@ -26,7 +19,7 @@ def get_products():
     return jsonify(products), 200
 
 # TODO: Implement GET /products/<id> route that returns a specific product by ID or 404
-@app.route("/products/<int:id>")
+@app.route("/products/<int:id>", methods=["GET"])
 def get_product_by_id(id):
     product = next((p for p in products if p["id"] == id), None)
     if product:
